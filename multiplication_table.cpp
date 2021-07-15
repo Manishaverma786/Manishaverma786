b0af25c3619073cb8bff2c4cf027fa37ad3cd214
Manishaverma786 <63356997+Manishaverma786@users.noreply.github.com>
2021-07-16 00:59:26 +0530

    Create multiplication_table.cpp

---
+++ multiplication_table.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,n;
+cout<<"enter the value for multiplication";
+cin>>n;
+for(i=1;i<=10;i++)
+{
+cout<<n<<" X "<<i<<" = "<<n*i<<endl;
+}}
